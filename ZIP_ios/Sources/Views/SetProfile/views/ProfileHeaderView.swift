0a5ad1ddde8d023f3710ebdb25a4935cf868ce6b
bumwoo <qjadn0914@naver.com>
2018-08-09 00:01:36 +0900

    정렬 추가

--- ZIP_ios/Sources/Views/SetProfile/views/ProfileHeaderView.swift
+++ ZIP_ios/Sources/Views/SetProfile/views/ProfileHeaderView.swift
@@ -9,6 +9,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 import RxDataSources
+import RxGesture
 import TLPhotoPicker
 import Kingfisher
 
@@ -20,8 +21,26 @@ final class ProfileHeaderView: UIView {
   var profiles = [UIImage?](repeating: nil, count: 5)
   var viewController: SetProfileViewController?
   
+  let datas = BehaviorRelay<[SectionOfCustomData]>(value: [])
   
   var currentIndex = 0
+  
+  lazy var dataSources = RxCollectionViewSectionedReloadDataSource<SectionOfCustomData>(configureCell: {[unowned self] ds,cv,idx, item in
+    let cell = cv.dequeueReusableCell(withReuseIdentifier: String(describing: ProfileViewCell.self), for: idx) as! ProfileViewCell
+    cell.item = item
+    cell.vc = self
+    cell.tag = idx.item
+    
+    cell
+      .profileImageAction
+      .map{_ in return ()}
+      .do(onNext:{self.currentIndex = idx.item})
+      .bind(onNext: self.pushImageSelectView)
+      .disposed(by: self.disposeBag)
+    
+    return cell
+  })
+  
   lazy var collectionView: UICollectionView = {
     let layout = UICollectionViewFlowLayout()
     layout.scrollDirection = .horizontal
@@ -36,34 +55,50 @@ final class ProfileHeaderView: UIView {
   override init(frame: CGRect) {
     super.init(frame: frame)
     addSubview(collectionView)
-    
     reload()
     
-    collectionView.rx
-      .itemSelected
-      .subscribe(onNext: {[weak self] index in
-        guard let `self` = self else {return}
-        let controller = TLPhotosPickerViewController()
-        var configure = TLPhotosPickerConfigure()
-        configure.allowedLivePhotos = true
-        configure.autoPlay = true
-        configure.maxSelectedAssets = 1
-        controller.configure = configure
-        controller.delegate = self
-        self.viewController?.present(controller, animated: true, completion: nil)
-      }).disposed(by: disposeBag)
+    datas.asDriver()
+      .drive(collectionView.rx.items(dataSource: dataSources))
+      .disposed(by: disposeBag)
     
     setNeedsUpdateConstraints()
   }
   
+  
   func reload(){
-    Observable.just(profiles)
-      .bind(to: collectionView.rx.items(
-        cellIdentifier: String(describing: ProfileViewCell.self),
-        cellType: ProfileViewCell.self)){
-          idx, model, cell in
-          cell.profileView.image = model
-      }.disposed(by: disposeBag)
+    imageSort()
+    Observable.just([SectionOfCustomData(items: profiles)])
+      .bind(to: datas)
+      .disposed(by: disposeBag)
+  }
+  
+  func pushImageSelectView(){
+    let controller = TLPhotosPickerViewController()
+    var configure = TLPhotosPickerConfigure()
+    configure.allowedLivePhotos = true
+    configure.autoPlay = true
+    configure.maxSelectedAssets = 1
+    controller.configure = configure
+    controller.delegate = self
+    self.viewController?.present(controller, animated: true, completion: nil)
+  }
+  
+  func imageSort(){
+    profiles.sort { (one, two) -> Bool in
+      if one != nil {
+        if two == nil {
+          return true
+        }else{
+          return false
+        }
+      }else{
+        if two != nil {
+          return false
+        }else{
+          return true
+        }
+      }
+    }
   }
   
   override func updateConstraints() {
@@ -90,13 +125,37 @@ extension ProfileHeaderView: TLPhotosPickerViewControllerDelegate{
   }
   
   func dismissPhotoPicker(withTLPHAssets: [TLPHAsset]) {
+    
     profiles[currentIndex] = withTLPHAssets.first?.fullResolutionImage
-    currentIndex += 1
     reload()
   }
 }
 
 class ProfileViewCell: UICollectionViewCell{
+  var vc: ProfileHeaderView?
+  let disposeBag = DisposeBag()
+  
+  var item: UIImage?{
+    didSet{
+      if let image = item{
+        profileView.image = image
+        cancelButton.isEnabled = true
+        cancelButton.isHidden = false
+      }else{
+        cancelButton.isEnabled = false
+        cancelButton.isHidden = true
+      }
+    }
+  }
+  
+  let cancelButton: UIButton = {
+    let button = UIButton()
+    button.setImage(#imageLiteral(resourceName: "deleteAll"), for: .normal)
+    button.isEnabled = false
+    button.isHidden = true
+    return button
+  }()
+  
   let profileView: UIImageView = {
     let imageView = UIImageView()
     imageView.layer.cornerRadius = 5
@@ -106,16 +165,47 @@ class ProfileViewCell: UICollectionViewCell{
     return imageView
   }()
   
+  lazy var profileImageAction = profileView.rx.tapGesture()
+    .when(.ended)
+    .share()
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     contentView.addSubview(profileView)
+    profileView.addSubview(cancelButton)
+    
+    cancelButton.rx.tap
+      .subscribe(onNext: {[unowned self] in
+        self.vc?.profiles[self.tag] = nil
+        self.vc?.reload()
+      }).disposed(by: disposeBag)
+    
+    
     profileView.snp.makeConstraints { (make) in
       make.edges.equalToSuperview().inset(UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5))
+    }
+    
+    cancelButton.snp.makeConstraints { (make) in
+      make.right.equalTo(profileView.snp.right)
+      make.top.equalTo(profileView.snp.top)
+      make.height.width.equalTo(20)
     }
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+struct SectionOfCustomData {
+  var items: [Item]
+}
+extension SectionOfCustomData: SectionModelType {
+  typealias Item = UIImage?
+  
+  init(original: SectionOfCustomData, items: [Item]) {
+    self = original
+    self.items = items
   }
 }
 
