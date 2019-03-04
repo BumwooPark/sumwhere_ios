76161c9fe917fad8a5504468502feb98a7d08d6c
bumwoo <qjadn0914@naver.com>
2019-03-04 20:06:29 +0900

    add NVActivity extension

--- ZIP_ios/Sources/Extensions/ActivityIndicator + Extension.swift
+++ ZIP_ios/Sources/Extensions/ActivityIndicator + Extension.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2019 park bumwoo. All rights reserved.
 //
 
-import Foundation
+import NVActivityIndicatorView
+
+
+extension NVActivityIndicatorPresenter{
+  func sumwhereStart(){
+    let activityData = ActivityData(size: CGSize(width: 50, height: 50), type: .circleStrokeSpin, color: #colorLiteral(red: 0.07450980392, green: 0.4823529412, blue: 0.7803921569, alpha: 1), backgroundColor: .clear)
+    self.startAnimating(activityData, NVActivityIndicatorView.DEFAULT_FADE_IN_ANIMATION)
+  }
+  
+  func sumwhereStop(){
+    self.stopAnimating(NVActivityIndicatorView.DEFAULT_FADE_OUT_ANIMATION)
+  }
+}
